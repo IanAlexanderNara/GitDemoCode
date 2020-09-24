04a7d2782b621f7232f7da0513c326eb991939c4
Ian Nara <nara@mymail.mines.edu>
2020-09-24 11:41:38 -0600

    Initial commit, created files and started making some method declarations

---
+++ src/ScoreTrakker.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+/**
+ * 
+ * @author Ian Nara
+ * @author Andrew Taylor
+ *
+ */
+public class ScoreTrakker {
+	//stores students
+	private ArrayList<Student> students;
+	
+	//constructor
+	public ScoreTrakker() {
+		super();
+		students = new ArrayList<Student>();
+	}
+	
+	private void loadDataFromFile(String filename){
+		
+	}
+	
+	private void printInOrder() {
+		
+	}
+	
+	public void processFiles() {
+		loadDataFromFile();
+		printInOrder();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
