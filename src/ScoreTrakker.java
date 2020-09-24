2bda8149fc9643ca823697ca06dd9aadb9e5ef83
Ian Nara <nara@mymail.mines.edu>
2020-09-24 12:07:50 -0600

    ScoreTrakker and Student works for processing single file

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.*;
 
 /**
@@ -16,22 +18,46 @@ public class ScoreTrakker {
 		students = new ArrayList<Student>();
 	}
 	
+	//takes a filename and loads data, storing as student objects
 	private void loadDataFromFile(String filename){
+		try {
+			FileReader reader = new FileReader(filename);
+			Scanner in = new Scanner(reader);
+			while(in.hasNextLine()) {
+				String name = in.nextLine();
+				int score = Integer.valueOf(in.nextLine());
+				Student student = new Student(name,score);
+				students.add(student);
+			}
+		}
+		catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
 		
 	}
 	
+	//sorts and prints in alphabetical order by name
 	private void printInOrder() {
-		
+		System.out.println("Student Score List");
+		Collections.sort(students);
+		for(Student s : students) {
+			System.out.println(s);
+		}
 	}
 	
 	public void processFiles() {
-		loadDataFromFile();
+		//get filename from user
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter file name");
+		String filename = in.nextLine();
+		//load data and print
+		loadDataFromFile(filename);
 		printInOrder();
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ScoreTrakker tracker = new ScoreTrakker();
+		tracker.processFiles();
 	}
 
 }
